9cabb42f1f2c6048c02565da9643f19d4de89499
jorgemg1414 <jorgemantin@gmail.com>
2025-12-23 08:19:38 -0600

    First commit

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,20 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animate-plus">Cargando Pokemons</h3>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1>¿Quién es este Pokémon?</h1>
+
+    <!-- Pokemon Picture -->
+    <PokemonPicture />
+    <!-- Pokemon options -->
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+</script>
